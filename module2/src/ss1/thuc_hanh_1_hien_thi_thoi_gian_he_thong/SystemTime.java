ad6deebfce0102261b8f08395f81758a8040c68e
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-02-15 16:51:58 +0700

    update thuc hanh

---
+++ module2/src/ss1/thuc_hanh_1_hien_thi_thoi_gian_he_thong/SystemTime.java
@@ -0,0 +1,11 @@
+package ss1.thuc_hanh_1_hien_thi_thoi_gian_he_thong;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
---
+++ module2/src/ss1/thuc_hanh_5_tinh_so_ngay_trong_thang/Main.java
@@ -0,0 +1,4 @@
+package ss1.thuc_hanh_5_tinh_so_ngay_trong_thang;
+
+public class Main {
+}
